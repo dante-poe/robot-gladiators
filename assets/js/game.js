e7ca9c8717d29a66affa81c1928e6104486da258
Sage-Poe <dante.s.poe@gmail.com>
2021-04-04 14:08:56 -0700

    Second Commit, console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt ("What is Your Robot's Name?");
+var name = "your name";
+
+
+function fight() {
+    window.alert("The Fight Has Begun!");
+}
+
+fight();
+
+console.log(name);
+console.log(playerName);
